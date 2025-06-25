c07ced7238b62aa3bc19ba244a4e8ac61577109e
Ravi Ram <raviram220204@gmail.com>
2025-06-26 02:22:11 +0530

    back again

--- server/server.js
+++ server/server.js
@@ -8,8 +8,6 @@ import usersRoute from "./routes/usersRoute.js";
 import productsRoute from "./routes/productsRoutes.js";
 import bidsRoute from "./routes/bidsRoute.js";
 import notificationsRoute from "./routes/notificationsRoute.js";
-import path from "path";
-import { fileURLToPath } from "url";
 
 connectDB();
 
@@ -22,18 +20,20 @@ app.use("/api/notifications", notificationsRoute);
 
 const port = process.env.PORT || 5000;
 
+// import path from "path";
+// import { fileURLToPath } from "url";
 
-// Simulate __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// // Simulate __dirname
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-// Deployment config
-app.use(express.static(path.join(__dirname, "client", "dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
-
+// // Deployment config
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/client/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 app.listen(port, () => {
   console.log(`Node/Express Server started on port ${port}`);
