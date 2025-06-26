3fe6f049f85e651a2727267c8c2f2c12581de054
Ravi Ram <raviram220204@gmail.com>
2025-06-26 19:51:59 +0530

    Updated axiosINstance

--- client/src/apicalls/axiosInstance.js
+++ client/src/apicalls/axiosInstance.js
@@ -2,7 +2,13 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  headers: {
-    authorization: `Bearer ${localStorage.getItem("token")}`,
-  },
+});
+
+// Dynamically attach token before each request
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.authorization = `Bearer ${token}`;
+  }
+  return config;
 });
